32b3ea9497d90210dfe6f212096c64f4faafbfb7
Adam Harwell <flux.adam@gmail.com>
2020-08-16 02:27:07 -0700

    Bump version

--- nparse.py
+++ nparse.py
@@ -47,7 +47,7 @@ from config.ui import SettingsWindow
 os.environ["QT_SCALE_FACTOR"] = str(app_config.qt_scale_factor / 100)
 
 # update check
-CURRENT_VERSION: str = "0.6.0-sharing"
+CURRENT_VERSION: str = "0.6.0-sharing2"
 if app_config.update_check:
     ONLINE_VERSION: str = get_version()
 else:
